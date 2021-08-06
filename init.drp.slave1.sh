2976f05926c768932fe90659d42b114515be5c65
openagora <52544513+openagora@users.noreply.github.com>
2021-08-06 16:47:10 -0400

    Creacion init.drp.slave1.sh
    creacion init.drp.slave1.sh

---
+++ init.drp.slave1.sh
@@ -0,0 +1,10 @@
+#curl -s https://raw.githubusercontent.com/openagora/ami-userdata/master/init.drp.slave1.sh | bash
+
+#busco el bucket DRP y lo clono
+/usr/bin/aws --profile oa-drp s3 sync s3://oa-drp-master s://oa-drp-slave1
+
+#una pausa antes de terminar
+sleep 300
+
+#una vez terminado lo anterior, modifico el scalingroup del drp y lo dejo en 0
+/usr/bin/aws --profile oa-drp autoscaling update-auto-scaling-group --auto-scaling-group-name oa-drp --min-size 0 --max-size 0 
