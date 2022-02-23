06347c918c5c9778956d974e90edf125a622cae9
openagora <52544513+openagora@users.noreply.github.com>
2022-02-23 20:18:04 -0300

    ephmeral solo cuando corresponde

--- init.sh
+++ init.sh
@@ -12,34 +12,34 @@ cd /openagora
 
 echo "SMALL" > /var/code/WORKER.SIZE
 yum -y install nvme-cli
-mkdir -p /mnt/ephemeral0
 
-
-/usr/bin/chown ec2-user:ec2-user /mnt/ephemeral0
 
 if /sbin/nvme -list | /bin/grep -q "Instance Storage" ; then
-#el working dir DEBE SER el ephemeral
-echo "LARGE" > /var/code/WORKER.SIZE
 
-EPHEMERAL_DISK=$(/sbin/nvme list | grep 'Instance Storage' | awk '{ print $1 }')
-/usr/sbin/mkfs.xfs $EPHEMERAL_DISK
-/usr/bin/mount -t xfs $EPHEMERAL_DISK /mnt/ephemeral0
+  mkdir -p /mnt/ephemeral0
+  /usr/bin/chown ec2-user:ec2-user /mnt/ephemeral0
 
-EPHEMERAL_UUID=$(sudo blkid -s UUID -o value $EPHEMERAL_DISK)
+  #el working dir DEBE SER el ephemeral
+  echo "LARGE" > /var/code/WORKER.SIZE
 
-mkdir -p -m 1777 /mnt/ephemeral0/tmp
+  EPHEMERAL_DISK=$(/sbin/nvme list | grep 'Instance Storage' | awk '{ print $1 }')
+  /usr/sbin/mkfs.xfs $EPHEMERAL_DISK
+  /usr/bin/mount -t xfs $EPHEMERAL_DISK /mnt/ephemeral0
 
-echo "UUID=$EPHEMERAL_UUID /mnt/ephemeral0 xfs defaults,nofail 0 2" |  tee -a /etc/fstab
-echo "/mnt/ephemeral0/tmp /tmp  none rw,noexec,nofail,bind 0 0" |  tee -a /etc/fstab
+  EPHEMERAL_UUID=$(sudo blkid -s UUID -o value $EPHEMERAL_DISK)
 
+  mkdir -p -m 1777 /mnt/ephemeral0/tmp
 
+  echo "UUID=$EPHEMERAL_UUID /mnt/ephemeral0 xfs defaults,nofail 0 2" |  tee -a /etc/fstab
+  echo "/mnt/ephemeral0/tmp /tmp  none rw,noexec,nofail,bind 0 0" |  tee -a /etc/fstab
+
+  mkdir -p /mnt/ephemeral0/codetmp
+  /usr/bin/chown ec2-user:ec2-user /mnt/ephemeral0/codetmp
+
+  /bin/ln -s  /mnt/ephemeral0/codetmp /var/code/tmp
+  /usr/bin/chown -h ec2-user:ec2-user /var/code/tmp
+  
 fi
-mkdir -p /mnt/ephemeral0/codetmp
-/usr/bin/chown ec2-user:ec2-user /mnt/ephemeral0/codetmp
-
-/bin/ln -s  /mnt/ephemeral0/codetmp /var/code/tmp
-/usr/bin/chown -h ec2-user:ec2-user /var/code/tmp
-
 
 #actualizo la instancia
 yum -y update --exclude=python*
@@ -66,9 +66,10 @@ mkdir -p /openagora/oatools
  
 #correccion de propietarios
 chown ec2-user:ec2-user -R /var/code
-chown ec2-user:ec2-user -R /mnt/ephemeral0/codetmp
 chown ec2-user:ec2-user -R /openagora
- 
+
+#suponiendo que existe
+chown ec2-user:ec2-user -R /mnt/ephemeral0/codetmp
  
 #Borro el bash_history
 cat /dev/null > ~/.bash_history && history -c
