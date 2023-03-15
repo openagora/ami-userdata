fe3110ce32d20e049e687ccecb27eb0d82850a07
openagora <52544513+openagora@users.noreply.github.com>
2023-03-15 17:34:44 -0300

    Update initlms.sh

--- initlms.sh
+++ initlms.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 # este script SOLO CORRE en el primer BOOTEO de una instancia, y no en los siguientes reboot
 
+
+sudo apt-get update
+sudo apt-get -y install s3fs
+
+
 #Ami no tiene /var/code
 mkdir -p /var/code
 
@@ -9,3 +14,5 @@ mkdir -p /var/code
 rm -rf /openagora
 mkdir -p /openagora
 cd /openagora
+
+
