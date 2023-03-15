def965e88855e73de4d174b9ecde3d8c0ecfa7cb
openagora <52544513+openagora@users.noreply.github.com>
2023-03-15 17:42:28 -0300

    Update initlms.sh

--- initlms.sh
+++ initlms.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -e
 # este script SOLO CORRE en el primer BOOTEO de una instancia, y no en los siguientes reboot
 
+# INICIO AMI
 
 sudo apt-get update
 sudo apt-get -y install s3fs
+mkdir /tmp/ssm
+cd /tmp/ssm
+wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+sudo dpkg -i amazon-ssm-agent.deb
+sudo systemctl enable amazon-ssm-agent
+
+# FIN AMI
 
 
 #Ami no tiene /var/code
