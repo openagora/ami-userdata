5d3905688a1b6458ea01c10b6584d7839d2fc0f9
Juan Torres <juanin.torres@gmail.com>
2023-03-16 13:09:07 -0300

    Update initlms.sh

--- initlms.sh
+++ initlms.sh
@@ -4,12 +4,20 @@
 # INICIO AMI
 
 sudo apt-get update
-sudo apt-get -y install s3fs
+sudo apt-get -y install s3fs jq
 mkdir /tmp/ssm
 cd /tmp/ssm
 wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
 sudo dpkg -i amazon-ssm-agent.deb
 sudo systemctl enable amazon-ssm-agent
+
+#sudo groupmod --new-name ec2-user bitnami-admins
+#sudo usermod -l ec2-user bitnami
+
+
+
+#sudo mkdir -p /mnt/s3fs
+#sudo chown ec2-user:ec2-user /mnt/s3fs 
 
 # FIN AMI
 
